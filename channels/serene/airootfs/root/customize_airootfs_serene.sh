39c3e64e583bd4d0ac29edd743732a4985f0ca22
mk-linux419 <m.k419sabuaka@gmail.com>
2020-08-17 20:20:18 +0900

    [update] Added License

--- channels/serene/airootfs/root/customize_airootfs_serene.sh
+++ channels/serene/airootfs/root/customize_airootfs_serene.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: GPL-3.0
+#
+# mk-linux419
+# Twitter: @fascoder_4
+# Email  : m.k419sabuaka@gmail.com
+#
+# Yamada Hayao
+# Twitter: @Hayao0819
+# Email  : hayao@fascode.net
+#
+# (c) 2019-2020 Fascode Network.
+#
+# customize_airootfs_serene.sh
+#
 
 echo "customize_airootfs.sh has been executed."
